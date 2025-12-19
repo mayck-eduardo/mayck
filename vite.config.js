9bb2bb5cb71d0740725fa956a85df05f2532ad8a
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-12-19 17:33:26 -0300

    adiciona página de blog e configura entrada no Vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         wip: resolve(__dirname, 'wip.html'),
+        blog: resolve(__dirname, 'blog.html'), // Adicionado
       },
     },
   },
