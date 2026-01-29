eea691a285ba577fad1d7077b47e622d2d756164
Mayck Eduardo <mayck_22.03@hotmail.com>
2026-01-29 11:20:52 -0300

    feat: add txt.html and update vite.config.js for new entry point
    - Added a new HTML file: txt.html
    - Updated vite.config.js to include txt.html as a build input
    - Set base path for deployment to '/mayck/'

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/mayck/', 
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         wip: resolve(__dirname, 'wip.html'),
         blog: resolve(__dirname, 'blog.html'), // Adicionado
+        txt: resolve(__dirname, 'txt.html'), // Adicionado
       },
     },
   },
