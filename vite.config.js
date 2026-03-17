2cddf38667be86c4dec825775b05e1e65e795b3f
Mayck Eduardo <mayck_22.03@hotmail.com>
2026-03-17 14:09:44 -0300

    feat: add página do contador

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         blog: resolve(__dirname, 'blog.html'),
         evofit: resolve(__dirname, 'evofit.html'),
         estoque: resolve(__dirname, 'estoque.html'),
+        contador: resolve(__dirname, 'contador.html'),
         'station-magic': resolve(__dirname, 'station-magic.html'),
         admin: resolve(__dirname, 'admin.html'),
         post: resolve(__dirname, 'post.html'),
